be33ec06c1f24b872c26ca649202c1bc3a0b058a
Zeth <38241145+zeth1998v@users.noreply.github.com>
2019-05-04 05:49:02 +0200

    Upload files

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Slackerz: Collaboration is everything</title>
+	<link href="https://fonts.googleapis.com/css?family=Poppins|Raleway:300,400|Roboto|Ubuntu" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="assets/css/init.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/navbar.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/index.css">
+</head>
+<body>
+	<div id="shapes">
+		<img id="shape1" src="assets/img/shapes/shape1.png">
+		<img id="shape2" src="assets/img/shapes/shape2.png">
+	</div>
+	<div id="navbar">
+		<div id="navLeft">
+			<img id="navLogo" src="">
+			<a href="index"><h1 id="navTitle">Slackerz<span id="navTitleSpan">.</span></h1></a>
+		</div>
+		<div id="navRight">
+			<ul class="navList" id="navList1">
+				<li><a href="#">Product</a></li>
+				<li><a href="#">Discover</a></li>
+				<li><a href="#">Platform</a></li>
+				<li><a href="#">Support</a></li>
+				
+			</ul>
+			<ul class="navList" id="navList2">
+				<li id="loginItem"><a href="#">Login</a></li>
+				<li id="signupItem"><a href="signup">Sign up</a></li>
+			</ul>
+		</div>
+	</div>
+	<div id="wrapper1">
+		<div id="leftWrapper1">
+			<h1>The collaboration software that moves work forward.</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua.</p>
+			<a href="signup">Sign up</a>
+		</div>
+		<div id="rightWrapper1">
+			<img src="assets/img/statusUpdate.png">
+		</div>
+	</div>
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,82 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Slackerz: Create Account</title>
+	<link href="https://fonts.googleapis.com/css?family=Poppins|Raleway:300,400|Roboto|Ubuntu" rel="stylesheet">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="assets/css/init.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/navbar.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/signup.css">
+</head>
+<body>
+	<div id="navbar">
+		<div id="navLeft">
+			<img id="navLogo" src="">
+			<a href="index"><h1 id="navTitle">Slackerz<span id="navTitleSpan">.</span></h1></a>
+		</div>
+		<div id="navRight">
+			<ul class="navList" id="navList1">
+				<li><a href="#">Product</a></li>
+				<li><a href="#">Discover</a></li>
+				<li><a href="#">Platform</a></li>
+				<li><a href="#">Support</a></li>
+				
+			</ul>
+			<ul class="navList" id="navList2">
+				<li id="loginItem"><a href="#">Login</a></li>
+				<li id="signupItem"><a href="signup">Sign up</a></li>
+			</ul>
+		</div>
+	</div>
+	<?php
+		if(isset($_GET['error'])){
+			if($_GET['error'] == "invalidemailusername"){
+				echo "<p class='errorField'>Invalid username and email!</p>";
+			} elseif($_GET['error'] == "invalidemail"){
+				echo "<p class='errorField'>Invalid email!</p>";
+			} elseif($_GET['error'] == "invalidusername"){
+				echo "<p class='errorField'>Invalid username!</p>";
+			} elseif($_GET['error'] == "passconfirm"){
+				echo "<p class='errorField'>Passwords does not match!</p>";
+			} elseif($_GET['error'] == "sqlerror"){
+				echo "<p class='errorField'>Server error!</p>";
+			} elseif($_GET['error'] == "userexist"){
+				echo "<p class='errorField'>Username is already taken!</p>";
+			}
+		}
+	?>
+	<div id="wrapper">
+		<div id="leftWrapper">
+			<img src="assets/img/profileData.png">
+		</div>
+		<div id="rightWrapper">
+			<form method="post" action="assets/inc/signup.inc.php">
+			<h1>Create Account</h1>
+			<div class="textBox" id="caUsername">
+				<label for="ca-username"><i class="fas fa-user"></i></label>
+				<input type="text" name="ca-username" required>
+			</div>
+			<div class="textBox" id="caEmail">
+				<label for="ca-email"><i class="fas fa-envelope"></i></label>
+				<input type="email" name="ca-email" required>
+			</div>
+			<div class="textBox" id="caPass">
+				<label for="ca-pass"><i class="fas fa-lock"></i></label>
+				<input type="password" name="ca-password" required>
+			</div>
+			<div class="textBox" id="caPass2">
+				<label for="ca-pass2"><i class="fas fa-lock-open"></i></label>
+				<input type="password" name="ca-password2" required>
+			</div>
+			<div class="buttonBox" id="caSub">
+				<button type="submit" name="ca-submit">Create Account</button>
+			</div>
+		</form>
+		</div>
+	</div>
+</body>
+</html>
